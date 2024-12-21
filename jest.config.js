f84b505f3855e41951d2ef1b6cc8385f1eb3541c
jonah slack <jcsslack@gmail.com>
2024-12-20 22:28:34 -0500

    chore: add project configuration files

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  testEnvironment: 'node',
+  roots: ['.'],
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+};
